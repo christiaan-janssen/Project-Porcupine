24c02fdc6d7bfeff6828aaed5623ec00f9f2a9fa
shidima <menicus@gmail.com>
2017-04-18 06:57:46 +0200

    Create inital project classes.
    Setup the world and tiles classes for our game.

---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Tile
+{
+    public enum TileType { Empty, Floor}
+
+    TileType type = TileType.Empty;
+
+    LooseObject looseObject;
+    InstalledObject installedObject;
+
+    World world;
+    int x, y;
+
+    public Tile(World world, int x, int y)
+    {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }
+}
